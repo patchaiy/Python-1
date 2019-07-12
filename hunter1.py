aef0786f59e4162968f58c2bb511a9fa5a9fc046
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-12 16:53:50 +0530

    Update hunter1.py

--- hunter1.py
+++ hunter1.py
@@ -1,12 +1,12 @@
-n=int(input())
-lis=[]
-for i in range(n):
-    lis.append(int(input()))
-set1=set(lis)
+num=int(input())
+lis1=[]
+for i in range(num):
+    lis1.append(int(input()))
+set1=set(lis1)
 count1=1
 ans=[]
 for i in set1:
-    count1=lis.count(i)
+    count1=lis1.count(i)
     if count1>1:
         ans.append(i)
     else:
