4a0da406ba53fe3c581cf47149f5b02bca53e6be
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-16 16:32:00 +0530

    Update pro33.py

--- pro33.py
+++ pro33.py
@@ -1,10 +1,6 @@
 s=input()
-lis=[]
 for i in range(1,len(s)):
-    o=ord(s[i])
-    lis.append(o)
-m=max(lis)
-m=chr(m)
-letter=s.find(m)
-ans=s[letter:]
+    if ord(s[i])>ord(s[0]):
+        ans=s[i:]
+        break
 print(ans)
