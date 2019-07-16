876ea87cfd539945bdb521212779d42e5f0bf7a9
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-16 16:03:36 +0530

    Create pro17.py

---
+++ pro17.py
@@ -0,0 +1,16 @@
+num=input()
+a=list(map(int,num.split()))
+k=a[1]
+h=input()
+flag=0
+s=list(map(int,h.split()))
+for i in range(0,len(s)-1):
+	for j in range(i+1,len(s)):
+		if s[i]+s[j]==k:
+			print("yes")
+			flag==1:
+			break
+	if flag==1:
+		break
+if flag!=1:
+	print("no")
