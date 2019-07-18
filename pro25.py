ac78d85f3f0bf95941ab0aca7432a78b1beea444
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-19 05:12:50 +0530

    Create pro25.py

---
+++ pro25.py
@@ -0,0 +1,12 @@
+num=int(input)
+lis=list(map(int,input().split))
+lis2=[]
+count=0
+for i in range(num-1):
+  if lis[i]>lis[i+1]:
+    count+=1
+  else:
+    lis2.append(count)
+    count=0
+m=max(lis2)
+print(m)
