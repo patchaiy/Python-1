3d30569aab9d31c891e5bcfc68579fa014660089
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-19 06:03:21 +0530

    Create pro53.py

---
+++ pro53.py
@@ -0,0 +1,8 @@
+s=input()
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+s=s.replace(" ","")
+lis=list(s)
+if l==lis:
+  print("yes")
+else:
+  print("no")
