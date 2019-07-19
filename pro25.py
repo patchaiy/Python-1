7ab9840f7d9b87e61515e958f750d4b3b9f46bea
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-19 05:32:12 +0530

    Update pro25.py

--- pro25.py
+++ pro25.py
@@ -1,10 +1,10 @@
-z=input()
-l1=list(map(int,input().split()))
+nm=input()
+lis1=list(map(int,input().split()))
 maximum=0
 i=0
-while(i<len(l1)-1):
+while(i<len(lis1)-1):
     count=0
-    while(i<len(l1)-1 and l1[i]<l1[i+1]):
+    while(i<len(lis1)-1 and lis1[i]<lis1[i+1]):
         count+=1
         i+=1
     if(count>maximum):
