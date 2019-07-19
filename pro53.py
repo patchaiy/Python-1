ca16d205f2842065faee92294b4ed1c95e207e37
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-19 06:05:11 +0530

    Update pro53.py

--- pro53.py
+++ pro53.py
@@ -2,6 +2,7 @@ s=input()
 l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 s=s.replace(" ","")
 lis=list(s)
+lis.sort()
 if l==lis:
   print("yes")
 else:
