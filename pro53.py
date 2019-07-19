cd6b0e1f2ce62a5987d3652e7494b100636dae91
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-19 06:11:19 +0530

    Update pro53.py

--- pro53.py
+++ pro53.py
@@ -3,7 +3,9 @@ s=s.lower()
 l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 s=s.replace(" ","")
 lis=list(s)
-lis.sort()
+se=set(lis)
+lis=list(se)
+lis=lis.sort()
 if l==lis:
   print("yes")
 else:
