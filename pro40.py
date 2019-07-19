2121544d14834ea405403e87b76e4f3d2aa94c93
SriLekha7312 <52819214+SriLekha7312@users.noreply.github.com>
2019-07-19 05:56:17 +0530

    Update pro40.py

--- pro40.py
+++ pro40.py
@@ -1,10 +1,9 @@
-s=input()
-a='dhoni'
-a=list(a)
-so=a.sort()
-s=list(s)
-an=s.sort()
-if so==an:
-  print("yes")
+st1="dhoni"
+st2=input()
+val1=list(set(st1)-set(st2))
+val2=list(set(st2)-set(st1))
+val=len(val1)+len(val2)
+if val==0:
+	print("yes")
 else:
-  print("no")
+	print("no")
